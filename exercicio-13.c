9cf36dc5b38b07b66d53715a0500a18c26714c95
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-02-19 23:01:17 -0400

    Create exercicio-13.c

---
+++ exercicio-13.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main()
+{
+    //Calcule e mostre o número de lâmpadas necessárias para iluminar um determinado cômodo de uma residência. Dados de entrada: a potência da lâmpada utilizada (em watts), as dimensões (largura e comprimento, em metros) do cômodo. Considere que a potência necessária é de 20 watts por metro quadrado.
+    
+}
