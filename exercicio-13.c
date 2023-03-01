54792d003c4123662e220ee8a9eb6dfa109ab6c7
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-03-01 14:56:44 -0400

    ex 13

--- exercicio-13.c
+++ exercicio-13.c
@@ -2,5 +2,11 @@
 void main()
 {
     //Calcule e mostre o número de lâmpadas necessárias para iluminar um determinado cômodo de uma residência. Dados de entrada: a potência da lâmpada utilizada (em watts), as dimensões (largura e comprimento, em metros) do cômodo. Considere que a potência necessária é de 20 watts por metro quadrado.
-    
+    float potencia, largura, comprimento, num_lampadas;
+    printf("\nInflorme a potencia da lampada a ser utilizada");
+    scanf("%f", &potencia);
+
+    printf("\nInforme a larguara do comodo");
+    scanf("%f", &largura);
+
 }
